85431ad44bd7c4f7382cf35efbf059d2c3dca7a9
Heverthon Yan <heverthon.yan@outlook.com>
2023-09-09 15:37:02 -0300

    Delete quest9.py

--- quest9.py
+++
@@ -1,9 +0,0 @@
-'''9) Escreva um programa para calcular e exibir a média ponderada de 2 notas dadas. (nota1= peso 6 e
-nota2= peso 4)'''
-
-nota1 = float(input("Digite sua primeira nota: "))
-nota2 = float(input("Digite sua segunda nota: "))
-peso1 = 6
-peso2 = 4
-media = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2)
-print("A média ponderada das notas é: " ,media)
