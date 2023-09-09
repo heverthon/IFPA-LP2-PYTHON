aaf0c1bbf4d6722d3b33dd2ba9148c24c382a20d
Heverthon Yan <heverthon.yan@outlook.com>
2023-09-09 15:37:22 -0300

    Delete quest7.py

--- quest7.py
+++
@@ -1,9 +0,0 @@
-'''7) Escreva um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
-que este sofreu um desconto de 10%.'''
-
-produto = input("Digite o nome do produto: ")
-preco = float(input("Digite o preço do produto: "))
-desconto = preco * 0.1
-novo_preco = preco - desconto
-
-print("O novo valor com desconto é: " , novo_preco)
