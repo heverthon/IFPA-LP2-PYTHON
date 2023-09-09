7fcdf8cae457b2034a11c20e522d65210e24e9da
Heverthon Yan <heverthon.yan@outlook.com>
2023-09-09 15:38:35 -0300

    Delete quest10 list3.py

--- quest10 list3.py
+++
@@ -1,5 +0,0 @@
-codigo = input("Digite o seu código de consumidor: ")
-preco_kw = float(input("Digite o preço do KW: "))
-quantidade_kw = float(input("Digite a quantidade do KW consumido: "))
-total = preco_kw * quantidade_kw
-print("O seu código de consumidor é: " ,codigo, "E você tem um total à pagar de: " ,total)
